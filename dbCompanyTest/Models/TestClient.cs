919785c777f5574676ea4c3f500eacefaaf4d76c
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-03 22:25:59 +0800

    a

--- dbCompanyTest/Models/TestClient.cs
+++ dbCompanyTest/Models/TestClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace dbCompanyTest.Models
 {
@@ -11,14 +12,16 @@ namespace dbCompanyTest.Models
             Orders = new HashSet<Order>();
             會員商品暫存s = new HashSet<會員商品暫存>();
         }
-
+        [Required]
         public string 客戶編號 { get; set; } = null!;
+        [Required]
         public string? 客戶姓名 { get; set; }
         public string? 客戶電話 { get; set; }
         public string? 身分證字號 { get; set; }
         public string? 縣市 { get; set; }
         public string? 區 { get; set; }
         public string? 地址 { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
         public string? 密碼 { get; set; }
         public string? 性別 { get; set; }
