40c269d189d6be4c304cab8a2eddde1d72442585
th9716 <55468263+th9716@users.noreply.github.com>
2023-01-03 17:01:38 +0800

    產品牆Controller

---
+++ dbCompanyTest/Controllers/ProductWallController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dbCompanyTest.Controllers
+{
+    public class ProductWallController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        //---------------------- Gary產品頁 ----------------------------
+    }
+}
