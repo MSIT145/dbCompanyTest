02c2ae8df0d6218471e9abdb61fa24566e73044f
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-04 20:38:52 +0800

    a

---
+++ dbCompanyTest/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dbCompanyTest.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public void loginSussess()
+        {
+
+        }
+    }
+}
