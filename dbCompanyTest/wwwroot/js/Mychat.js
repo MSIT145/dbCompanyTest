152b5a1167339d4bfc4ed51c96c3ed1fd5cae2b3
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-04 17:30:55 +0800

    a

---
+++ dbCompanyTest/wwwroot/js/Mychat.js
@@ -0,0 +1,12 @@
+﻿$(`.chatBody`).hide(500);
+$(`.chatMain`).hover(function () {
+    $(`.eyes-left`).toggleClass(`eyes-left-hover`);
+    $(`.eyes-right`).toggleClass(`eyes-right-hover`);
+    $(`.mouth`).toggleClass(`mouth-hover`);
+}, function () {
+    $(`.eyes-left`).toggleClass(`eyes-left-hover`);
+    $(`.eyes-right`).toggleClass(`eyes-right-hover`);
+    $(`.mouth`).toggleClass(`mouth-hover`);
+}).click(function () {
+    $(`.chatBody`).toggle(500);
+});
