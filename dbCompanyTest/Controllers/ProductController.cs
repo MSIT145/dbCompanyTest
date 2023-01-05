a0d6d8be574ddb50b299fa2842077bff5b4690f4
SkyLin8012 <fagw.sky@gmail.com>
2023-01-05 20:29:29 +0800

    20230105上傳
    sky_20:39_新增產品一覽

---
+++ dbCompanyTest/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using dbCompanyTest.Models;
+using dbCompanyTest.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dbCompanyTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        dbCompanyTestContext db = new dbCompanyTestContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult showlist()
+        {
+            var data =from  p in db.Products.ToList()
+                      join pt in db.ProductsTypeDetails on p.商品分類id equals pt.商品分類id
+                      join s in db.商品鞋種s on p.商品鞋種id equals s.商品鞋種id
+                      select new BACK_ProductViewModels 
+                      {
+                            商品編號id  = p.商品編號id,
+                            上架時間 =p.上架時間,
+                            商品名稱 = p.商品名稱,
+                            商品價格 = p.商品價格,
+                            商品介紹 = p.商品介紹,
+                            商品材質  = p.商品材質,
+                            商品重量  = p.商品重量,
+                            商品成本 = p.商品成本,
+                            商品是否有貨 = p.商品是否有貨,
+                            商品分類  = pt.商品分類名稱,
+                            商品鞋種 = s.鞋種,
+                            商品是否上架  = p.商品是否上架
+                       };
+
+            return Json(new { data });
+        }
+    }
+}
---
+++ dbCompanyTest/ViewModels/BACK_ProductViewModels.cs
@@ -0,0 +1,18 @@
+﻿namespace dbCompanyTest.ViewModels
+{
+    public class BACK_ProductViewModels
+    {
+        public int 商品編號id { get; set; }
+        public string? 上架時間 { get; set; }
+        public string? 商品名稱 { get; set; }
+        public decimal? 商品價格 { get; set; }
+        public string? 商品介紹 { get; set; }
+        public string? 商品材質 { get; set; }
+        public int? 商品重量 { get; set; }
+        public decimal? 商品成本 { get; set; }
+        public bool? 商品是否有貨 { get; set; }
+        public string? 商品分類 { get; set; }
+        public string? 商品鞋種 { get; set; }
+        public bool? 商品是否上架 { get; set; }
+    }
+}
