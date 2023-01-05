dc4c6c2d5248ab19e5cbae1d7b6e30d13d6b5090
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-05 21:48:03 +0800

    修正性別與縣市區

--- dbCompanyTest/wwwroot/js/City_Data.js
+++ dbCompanyTest/wwwroot/js/City_Data.js
@@ -5,15 +5,20 @@ $("#city").change(function () {
     area(Myarea);
 })
 $(`#city`).val($(`#q1`).val());
+console.log($(`#q1`).val());
 if ($(`#q1`).val()) {
-    area($(`#city`).index() + 1);
+    console.log($(`#city option:selected`).index());
+    area($(`#city option:selected`).index());
+}
+if ($(`#sexvalue`).val()) {
+    $(`#sex`).val($(`#sexvalue`).val().trim());
 }
 
 function area(a) {
     $("#town").empty();
     $.each(data[a].districts, function (j, j_value) { $("#town").append(`<option>${j_value.name}</option>`) })
     if ($(`#q2`).val()) {
-        $(`#town`).val($(`#q2`).val());
+        $(`#town`).val($(`#q2`).val().trim());
         console.log($(`#q2`).val());
     }
 }
