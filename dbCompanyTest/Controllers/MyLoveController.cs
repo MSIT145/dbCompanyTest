8c775e7623654b8b5fc6c52de12ef91bea323305
Gary.Tsai <az17092145@gmail.com>
2023-01-05 18:59:23 +0800

    1/5gary
    1/5gary

---
+++ dbCompanyTest/Controllers/MyLoveController.cs
@@ -0,0 +1,15 @@
+﻿using dbCompanyTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dbCompanyTest.Controllers
+{
+    public class MyLoveController : Controller
+    {
+        dbCompanyTestContext _context=new dbCompanyTestContext();
+        public IActionResult Index()
+        {
+            var data = _context.會員商品暫存s.Select(x=>x).ToList().Where(y=>y.購物車或我的最愛!=true&&y.客戶編號.Contains("CL1-00376"));
+            return View(data);
+        }
+    }
+}
