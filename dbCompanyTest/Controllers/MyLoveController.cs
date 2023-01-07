0b53027428d05fe42125cf1acf3aec8b9e4ffd33
Gary.Tsai <az17092145@gmail.com>
2023-01-07 19:19:53 +0800

    1/7gary
    1/7gary

--- dbCompanyTest/Controllers/MyLoveController.cs
+++ dbCompanyTest/Controllers/MyLoveController.cs
@@ -1,5 +1,7 @@
 ﻿using dbCompanyTest.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Xml.Linq;
 
 namespace dbCompanyTest.Controllers
 {
@@ -10,6 +12,11 @@ namespace dbCompanyTest.Controllers
         {
             var data = _context.會員商品暫存s.Select(x=>x).ToList().Where(y=>y.購物車或我的最愛!=true&&y.客戶編號.Contains("CL1-00376"));
             return View(data);
+        }
+        public IActionResult Delete(int? id)
+        {
+
+            return Content("Hello" + id, "text/html", Encoding.UTF8);
         }
     }
 }
--- dbCompanyTest/wwwroot/js/favorites.js
+++ dbCompanyTest/wwwroot/js/favorites.js
@@ -1,3 +1,1 @@
-﻿$(`.purchase-del`).click(()=>{
-    $
-})
+﻿
