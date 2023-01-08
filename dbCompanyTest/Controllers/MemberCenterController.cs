046875a2db41d02c63d5e86d7a9aa39aaa5153b0
th9716 <lacey04849@gmail.com>
2023-01-08 20:57:24 +0800

    0108_yan

---
+++ dbCompanyTest/Controllers/MemberCenterController.cs
@@ -0,0 +1,36 @@
+﻿using dbCompanyTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dbCompanyTest.Controllers
+{
+    public class MemberCenterController : Controller
+    {
+        dbCompanyTestContext _context = new dbCompanyTestContext();
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult memberInfo(string? email)
+        {
+            var data = from c in _context.TestClients
+                       where c.Email == email
+                       select c;
+            return View();
+        }
+
+        public IActionResult orderInfo()
+        {
+
+            return View();
+        }
+
+        public IActionResult orderInfoDetail()
+        {
+
+            return View();
+        }
+
+    }
+}
