e79e000e9e0537e3ee303cc609caa80ae12424f5
th9716 <55468263+th9716@users.noreply.github.com>
2023-01-09 18:03:52 +0800

    0109_yan

--- dbCompanyTest/Controllers/MemberCenterController.cs
+++ dbCompanyTest/Controllers/MemberCenterController.cs
@@ -8,13 +8,13 @@ namespace dbCompanyTest.Controllers
         dbCompanyTestContext _context = new dbCompanyTestContext();
         public IActionResult Index()
         {
-
+            
             return View();
         }
 
         public IActionResult memberInfo(string id)
         {
-            id = "CL1-00376";
+            
             TestClient client = _context.TestClients.FirstOrDefault(x => x.客戶編號 == id);
             if(client != null)
             {
@@ -23,13 +23,15 @@ namespace dbCompanyTest.Controllers
             return View();
         }
 
-        public IActionResult orderInfo()
+        public IActionResult orderInfo(string id)
         {
-
-            return View();
+            var data = from c in _context.Orders
+                       where c.客戶編號 ==id
+                       select c;
+            return View(data);
         }
 
-        public IActionResult orderInfoDetail()
+        public IActionResult orderInfoDetail(string id)
         {
 
             return View();
