c0815b913d7152bbaf0d1652d394b86c7c0b2101
En221177 <88370443+En221177@users.noreply.github.com>
2023-01-10 18:58:23 +0800

    En back_order_list

--- dbCompanyTest/Controllers/Staff_HomeController.cs
+++ dbCompanyTest/Controllers/Staff_HomeController.cs
@@ -2,7 +2,9 @@
 using dbCompanyTest.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
 using System.Security.Principal;
+using static NuGet.Packaging.PackagingConstants;
 
 namespace dbCompanyTest.Controllers
 {
@@ -14,11 +16,10 @@ namespace dbCompanyTest.Controllers
             string stfNum = HttpContext.Session.GetString("Account");
             var stf = _context.TestStaffs.FirstOrDefault(c => c.員工編號 == stfNum);
 
-            var datas = from c in _context.Orders
-                        where c.訂單狀態 == "待出貨"
-                        select c;
-            var test = datas.ToList();
             
+            //var datas = _context.Orders.Where(c => c.訂單狀態 == "待出貨").Join(_context.OrderDetails,
+            //    c=>c.訂單編號, o=>o.訂單編號, ( c ,o) =>c.訂單編號).Join(_context.ProductDetails, c=>c.);
+
             ViewBag.acc = $"{stfNum} {stf.員工姓名} 您好!"; 
             return View();
         }
@@ -49,5 +50,32 @@ namespace dbCompanyTest.Controllers
             HttpContext.Session.Remove("Account");
             return RedirectToAction("login");
         }
+
+
+        //==================================================
+        public IActionResult LoadSheeplist()
+        {
+            var datas = from c in _context.Orders
+                        join o in _context.OrderDetails on c.訂單編號 equals o.訂單編號
+                        join a in _context.ProductDetails on o.Id equals a.Id
+                        join b in _context.ProductsSizeDetails on a.商品尺寸id equals b.商品尺寸id
+                        join d in _context.ProductsColorDetails on a.商品顏色id equals d.商品顏色id
+                        join e in _context.Products on a.商品編號id equals e.商品編號id
+                        where c.訂單狀態 == "待出貨"
+                        select new ViewModels.Cback_order_list
+                        {
+                            訂單編號 = c.訂單編號,
+                            客戶編號 = c.客戶編號,
+                            送貨地址 = c.送貨地址,
+                            訂單狀態 = c.訂單狀態,
+                            商品名稱 = e.商品名稱,
+                            尺寸種類 = b.尺寸種類,
+                            色碼 = d.色碼,
+                            商品數量 = (int)o.商品數量
+                        };
+            var test = datas.ToList();
+            return Json(datas);
+        }
+
     }
 }
