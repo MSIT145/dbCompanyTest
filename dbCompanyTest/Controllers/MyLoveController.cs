ac12323f2d725ae4021d1bf9440a46168d1d9d2a
Gary.Tsai <az17092145@gmail.com>
2023-01-11 20:07:27 +0800

    1/11gary
    1/11gary

--- dbCompanyTest/Controllers/MyLoveController.cs
+++ dbCompanyTest/Controllers/MyLoveController.cs
@@ -12,10 +12,31 @@ namespace dbCompanyTest.Controllers
     {
         dbCompanyTestContext _context=new dbCompanyTestContext();
         List<CarViewModels> list = null;
+        List<會員商品暫存> MyLovelist = null;
         public IActionResult Index()
         {
-            var data = _context.會員商品暫存s.Select(x=>x).ToList().Where(y=>y.購物車或我的最愛!=true&&y.客戶編號.Contains("CL2-00667"));
-            return View(data);
+            if (HttpContext.Session.Keys.Contains(CDittionary.SK_USE_FOR_LOGIN_USER_SESSION))
+            {
+
+                if (HttpContext.Session.Keys.Contains(CDittionary.SK_USE_FOR_MYLOVE_SESSION_NEW))
+                {
+                    string json = HttpContext.Session.GetString(CDittionary.SK_USE_FOR_MYLOVE_SESSION_NEW);
+                    MyLovelist = JsonSerializer.Deserialize<List<會員商品暫存>>(json);
+                    var data = _context.會員商品暫存s.Select(x => x).ToList().Where(y => y.購物車或我的最愛 != true && y.客戶編號.Contains("CL2-00667"));
+                    MyLovelist.AddRange(data.ToList());
+                    return View(MyLovelist);
+                }
+                else 
+                { 
+                var data = _context.會員商品暫存s.Select(x => x).ToList().Where(y => y.購物車或我的最愛 != true && y.客戶編號.Contains("CL2-00667"));
+                return View(data.ToList());
+                }
+            }
+            else 
+            {
+                return RedirectToAction(Url.Content("~/Login/Login"));
+            }
+
         }
         public IActionResult Delete(int? id)
         {
@@ -30,21 +51,21 @@ namespace dbCompanyTest.Controllers
                 return Content("沒有這筆資料");
             }
         }
-        public IActionResult JoinCart(string proname,string procolor,string prosize,int count) 
+        public IActionResult JoinCart() 
         {
-            if (HttpContext.Session.Keys.Contains(CDittionary.SK_USE_FOR_SHOPPING_CAR_SESSION))
-            {
-                string json = HttpContext.Session.GetString(CDittionary.SK_USE_FOR_SHOPPING_CAR_SESSION);
-                list = JsonSerializer.Deserialize<List<CarViewModels>>(json);
-            }
-            else 
-            {
-                list = new List<CarViewModels>();
+            //if (HttpContext.Session.Keys.Contains(CDittionary.SK_USE_FOR_SHOPPING_CAR_SESSION))
+            //{
+            //    string json = HttpContext.Session.GetString(CDittionary.SK_USE_FOR_SHOPPING_CAR_SESSION);
+            //    list = JsonSerializer.Deserialize<List<CarViewModels>>(json);
+            //}
+            //else 
+            //{
+            //    list = new List<CarViewModels>();
 
             
             
-            }
-            return Content("成功加入購物車"+proname+procolor+prosize+count);
+            //}
+            return Content("成功加入購物車");
         }
     }
 }
--- dbCompanyTest/Models/CDittionary.cs
+++ dbCompanyTest/Models/CDittionary.cs
@@ -4,5 +4,7 @@
     {
         public static readonly string SK_USE_FOR_SHOPPING_CAR_SESSION="SK_USE_FOR_SHOPPING_CAR_SESSION";
         public static readonly string SK_USE_FOR_LOGIN_USER_SESSION = "SK_USE_FOR_LOGIN_USER_SESSION";
+        public static readonly string SK_USE_FOR_MYLOVE_SESSION_NEW = "SK_USE_FOR_MYLOVE_SESSION";
+        public static readonly string SK_USE_FOR_MYLOVE_SESSION_OLD = "SK_USE_FOR_MYLOVE_SESSION";
     }
 }
