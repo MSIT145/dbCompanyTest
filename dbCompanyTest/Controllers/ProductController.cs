de483fa257dfd22422ccf3de3309f3c07bda34d0
SkyLin8012 <fagw.sky@gmail.com>
2023-01-12 21:29:22 +0800

    sky 20230112

--- dbCompanyTest/Controllers/ProductController.cs
+++ dbCompanyTest/Controllers/ProductController.cs
@@ -19,8 +19,46 @@ namespace dbCompanyTest.Controllers
         }
         public IActionResult CreateProduct(string id,string time,string name,string price,string introduce ,string material,string weight,string cost ,string typeid,string shoeid,string instock,string onshelves)
         {
+            //可以先做後端檢查(time、價格、成本不能亂填)
 
-            return Json(name);
+
+            //檢查後可以寫入Product Model內存入資料庫
+            try
+            {
+                Product pro = new Product();
+                pro.上架時間 = time;
+                pro.商品名稱 = name;
+                pro.商品價格 = Convert.ToDecimal(double.TryParse(price, out double _price) ? _price : 0);
+                pro.商品介紹 = introduce;
+                pro.商品材質 = material;
+                pro.商品重量 = Int32.TryParse(weight, out int _weight) ? _weight : 0;
+                pro.商品成本 = Convert.ToDecimal(double.TryParse(cost, out double _cost) ? _cost : 0);
+                pro.商品分類id = Int32.TryParse(typeid, out int _typeid) ? _typeid : 0;
+                pro.商品鞋種id = Int32.TryParse(shoeid, out int _shoeid) ? _shoeid : 0;
+                pro.商品是否有貨 = bool.TryParse(instock, out bool _instock) ? _instock : false;
+                pro.商品是否上架 = bool.TryParse(onshelves, out bool _onshelves) ? _onshelves : false;
+
+                db.Products.Add(pro);
+                db.SaveChanges();
+                return Json("新增成功");
+            }
+            catch
+            {
+                return Json("失敗");
+            }
+        }
+
+        public IActionResult Pro_Edit(string id) {
+            if (!string.IsNullOrEmpty(id))
+            {
+                var pro = db.Products.Where(p => p.商品編號id == Int32.Parse(id)).FirstOrDefault();
+                if (pro != null)
+                {
+                    return Json(pro);
+                }
+                return Json(null);
+            }             
+            return Json(null);        
         }
 
         public IActionResult GetTyteD()
