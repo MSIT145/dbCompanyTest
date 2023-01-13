42a3780ab559555d5def308ba73522eca01d8227
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-13 11:54:10 +0800

    1/13 service

---
+++ dbCompanyTest/Controllers/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dbCompanyTest.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Service()
+        {
+            return View();
+        }
+    }
+}
