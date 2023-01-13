785cfe625d174b312d32e441467d643c08ab0d02
SkyLin8012 <fagw.sky@gmail.com>
2023-01-13 20:31:59 +0800

    sky_20230113
    20:31
    新增PRODUCT
    新刪修
    沒有驗證
    缺細項crud

--- dbCompanyTest/Controllers/ProductController.cs
+++ dbCompanyTest/Controllers/ProductController.cs
@@ -17,6 +17,65 @@ namespace dbCompanyTest.Controllers
 
             return PartialView();
         }
+
+        public IActionResult DeleteProduct(string? id)
+        {
+            int _id = 0;
+            if (string.IsNullOrEmpty(id)|| !(Int32.TryParse(id, out _id)))
+                return Json("錯誤_傳輸id資料異常");
+
+            IEnumerable<ProductDetail> data = db.ProductDetails.Where(pd => pd.商品編號id == _id).ToList();
+            if(data.Count() !=0)
+                return Json("警告_商品尚有明細,不能刪除");
+
+            var Pro = db.Products.FirstOrDefault(p => p.商品編號id == _id);
+            if(Pro == null)
+                return Json("錯誤_沒有此項商品");
+            string name = Pro.商品名稱;
+            db.Products.Remove(Pro);
+            db.SaveChanges();
+        
+            return Json($"商品{name}刪除成功");
+        }
+
+
+        public IActionResult EditProduct(string id, string time, string name, string price, string introduce, string material, string weight, string cost, string typeid, string shoeid, string instock, string onshelves)
+        {
+            //可以先做後端檢查(time、價格、成本不能亂填)
+
+
+            //檢查後可以寫入Product Model內存入資料庫
+            int _id = 0;
+
+            if (string.IsNullOrEmpty(id)|| !(Int32.TryParse(id,out _id)))
+                return Json("失敗!商品id沒資料!!");
+
+            try
+            {
+                var pro = db.Products.FirstOrDefault(p => p.商品編號id == _id);
+                if (pro == null)
+                    return Json("失敗!找不到資料");
+                
+                    pro.上架時間 = time;
+                    pro.商品名稱 = name;
+                    pro.商品價格 = Convert.ToDecimal(double.TryParse(price, out double _price) ? _price : 0);
+                    pro.商品介紹 = introduce;
+                    pro.商品材質 = material;
+                    pro.商品重量 = Int32.TryParse(weight, out int _weight) ? _weight : 0;
+                    pro.商品成本 = Convert.ToDecimal(double.TryParse(cost, out double _cost) ? _cost : 0);
+                    pro.商品分類id = Int32.TryParse(typeid, out int _typeid) ? _typeid : 0;
+                    pro.商品鞋種id = Int32.TryParse(shoeid, out int _shoeid) ? _shoeid : 0;
+                    pro.商品是否有貨 = bool.TryParse(instock, out bool _instock) ? _instock : false;
+                    pro.商品是否上架 = bool.TryParse(onshelves, out bool _onshelves) ? _onshelves : false;
+                    db.SaveChanges();
+                    return Json("修改成功!");
+            }
+            catch
+            {
+                return Json("失敗!資料庫寫入異常");
+            }
+        }
+
         public IActionResult CreateProduct(string id,string time,string name,string price,string introduce ,string material,string weight,string cost ,string typeid,string shoeid,string instock,string onshelves)
         {
             //可以先做後端檢查(time、價格、成本不能亂填)
