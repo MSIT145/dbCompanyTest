7e8c90f5eddc5aa73e1e77dc38e5038cda1a95c5
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-15 13:39:10 +0800

    1/15

--- dbCompanyTest/Models/TestClient.cs
+++ dbCompanyTest/Models/TestClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace dbCompanyTest.Models
 {
@@ -13,12 +14,14 @@ namespace dbCompanyTest.Models
         }
 
         public string 客戶編號 { get; set; } = null!;
+        
         public string? 客戶姓名 { get; set; }
         public string? 客戶電話 { get; set; }
         public string? 身分證字號 { get; set; }
         public string? 縣市 { get; set; }
         public string? 區 { get; set; }
         public string? 地址 { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
         public string? 密碼 { get; set; }
         public string? 性別 { get; set; }
