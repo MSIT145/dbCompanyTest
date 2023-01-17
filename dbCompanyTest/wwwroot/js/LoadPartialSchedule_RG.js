aea263f4ee2c119ded6fbd49b1bffb1dca4a0426
En221177 <y.joseph0217@gmail.com>
2023-01-18 03:18:19 +0800

    En

--- dbCompanyTest/wwwroot/js/LoadPartialSchedule_RG.js
+++ dbCompanyTest/wwwroot/js/LoadPartialSchedule_RG.js
@@ -14,7 +14,30 @@
 //            <td id="time_ecu"></td>
 //            <td id="com_ecu"></td>
 //            <td id="sin_ecu"></td>
-var split_str_dpm = $(`#inp_whostart`).text().indexOf(' ');
-var str_dpm = $(`#inp_whostart`).text().substr(0, split);
-var split_ecu_dpm = $(`#sele_executor`).text().indexOf(' ');
-var str_ecu = $(`#sele_executor`).text().substr(0, split);
+//var split_str_dpm = $(`#inp_whostart`).text().indexOf(' ');
+//var str_dpm = $(`#inp_whostart`).text().substr(0, split);
+//var split_ecu_dpm = $(`#sele_executor`).text().indexOf(' ');
+//var str_ecu = $(`#sele_executor`).text().substr(0, split);
+
+//$("#dpm_str").append($("#inp_whostart").val());
+//$("#man_str").text($("#inp_whostart").val());
+//$("#time_str").text($("#inp_starttime").val());
+//$("#com_str").text();
+//$("#sin_str").val(`敬陳`);
+
+
+//$("#123").append(`<tr>
+//            <td id="dpm_str"></td>
+//            <td id="man_str"></td>
+//            <td id="time_str"></td>
+//            <td id="com_str"></td>
+//            <td id="sin_str">敬陳</td>
+//        </tr>
+//         <tr>
+//            <td id="dpm_ecu"></td>
+//            <td id="man_ecu"></td>
+//            <td id="time_ecu"></td>
+//            <td id="com_ecu"></td>
+//            <td id="sin_ecu"></td>
+//        </tr>`);
+
