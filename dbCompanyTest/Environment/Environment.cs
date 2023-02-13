24d38d9147c0498d6465adbc0a372c83b51b330d
LuMark0932 <mk219961201@hotmail.com>
2023-02-13 15:15:28 +0800

    0213LU
    order

--- dbCompanyTest/Environment/Environment.cs
+++ dbCompanyTest/Environment/Environment.cs
@@ -5,9 +5,9 @@ namespace dbCompanyTest.Environment
 {
     public class Environment
     {
-        //string apiKey = "2LU0i8A48bax1cQNKoRH5OFwfOG_43RaqcDa3yYViTPCgHtG7";
-        string apiKey = "2Lfh6JnTJCDuc3ES58TS4RXDREl_4GWqEQHoZZwEoKEFDTmg5";//LU
-        
+        string apiKey = "2LU0i8A48bax1cQNKoRH5OFwfOG_43RaqcDa3yYViTPCgHtG7";
+        /*string apiKey = "2Lfh6JnTJCDuc3ES58TS4RXDREl_4GWqEQHoZZwEoKEFDTmg5";*///LU
+
         bool open = true;
         internal string useEnvironment
         {
