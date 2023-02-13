dee2471ff212e63285be51be18cd24623961b1d6
En221177 <88370443+En221177@users.noreply.github.com>
2023-02-13 20:46:44 +0800

    En

--- dbCompanyTest/Hubs/chatHub.cs
+++ dbCompanyTest/Hubs/chatHub.cs
@@ -86,7 +86,7 @@ namespace dbCompanyTest.Hubs
                     {
                         user client = userList.FirstOrDefault(x => x.LineID == ClientID);
                         //傳送Line訊息方法
-                        new LineBot().SendLineMessage(client.LineID.Substring(4, client.LineID.Count() - 5), message);
+                        new LineBot().SendLineMessage(client.LineID.Substring(4, client.LineID.Count() - 4), message);
                     }
                 }
             }
