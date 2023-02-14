00af4225236b2d6e1700ed9dd12cea6231752abd
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-02-14 13:54:35 +0800

    2/14

--- dbCompanyTest/Environment/Environment.cs
+++ dbCompanyTest/Environment/Environment.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Policy;
+﻿using System.Drawing.Imaging;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
 using System.Text.Json;
 
 namespace dbCompanyTest.Environment
@@ -25,7 +28,22 @@ namespace dbCompanyTest.Environment
                             if (root.endpoints.Count == 0)
                                 return "https://localhost:7100";
                             else
+                            {
+                                string LineBotKey = "CiB9XbeXDnIXgfN8u7zbtIFGkaxP+VXghErm0tE/bntZJ6M9VZrIKvxUoLT2/38sLsDIXthopd+NwlcX/DT+LJKuOMUp9TJ/VlqVlcrsWjp1cjwFDzaL/2KcN3b+vNRgnP83LrM+iA6QYkFt/VqKiAdB04t89/1O/w1cDnyilFU=";
+                                var data = new Dictionary<string, string>()
+                                {
+                                    { "endpoint", root.endpoints[0].public_url+"/api/LineBot/Webhook" }
+                                };
+                                HttpClient Line = new HttpClient();
+                                HttpRequestMessage lineRequest = new HttpRequestMessage(new HttpMethod("Put"), "https://api.line.me/v2/bot/channel/webhook/endpoint")
+                                {
+                                    Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json")
+                                };
+                                Line.DefaultRequestHeaders.Add("Authorization", "Bearer " + LineBotKey);
+                                var lineResponse = Line.SendAsync(lineRequest).Result;
+                                var a = lineResponse.Content.ReadAsStringAsync().Result;
                                 return root.endpoints[0].public_url;
+                            }
                         }
                     }
                 else
